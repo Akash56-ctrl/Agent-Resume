06f2a57ad26714d896b541fb56cee72d7755f342
Akash Acharya <akashacharya17103@gmail.com>
2026-07-31 09:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -184,20 +184,13 @@ Show atleast Top 10-20 results with direct apply link"""
 # code = get_jobs(agent)
 # DISPLAY(HTML(code))
 
+
 if st.button("Generate Resume"):
-    with st.spinner("Agent Running"):
-        code=main_agent(agent,user_info)
-        st.html(
-            code,
-            width="stretch",
-            unsafe_allow_javascript=True
-        )
-
-        st.divider()  # to give horizontal divider
-
-        job_code=get_jobs(agent,location,profile)
-        st.html(
-            job_code,
-            width="stretch",
-            unsafe_allow_javascript=True
-        )
+           with st.spinner("Agent Running"):
+                      code = main_agent(agent,user_info)
+                      st.html(code , width="stretch" , 
+                              unsafe_allow_javascript=True)
+                      st.divider()  # to give horizontal div
+                      job_code = get_jobs(agent,location,profile)
+                      st.html(job_code , width="stretch" , 
+                              unsafe_allow_javascript=True)
