b613bacc739ff38aff87fc7ab0b5a8934783542a
Akash Acharya <akashacharya17103@gmail.com>
2026-07-31 09:43:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,160 +29,152 @@ st.image("https://raw.githubusercontent.com/Akash56-ctrl/Agent-Resume/refs/heads
 st.sidebar.title("Fill Important details")
 st.sidebar.image("https://raw.githubusercontent.com/Akash56-ctrl/Agent-Resume/refs/heads/main/bg.png")
 
-#STEP 1 API KEYS
-GOOGLE_API_KEY = st.sidebar.text_input("Gemini-API",type ="password")
-GROQ_API_KEY = st.sidebar.text_input("Groq-API",type ="password")
-TAVILY_API_KEY = st.sidebar.text_input("Tavily-API",type ="password")
+# ========API KEYS============# 
+# Step 3 API keys
+TAVILY_API_KEY = st.sidebar.text_input("Tavily-API",type = "password")
+GROQ_API_KEY = st.sidebar.text_input("Groq-API",type = "password")
+GOOGLE_API_KEY = st.sidebar.text_input("Gemini-API",type = "password")
 
-all_API =[TAVILY_API_KEY,GROQ_API_KEY,
-          GOOGLE_API_KEY ]
+all_API = [TAVILY_API_KEY,GROQ_API_KEY,
+           GOOGLE_API_KEY ]
 if not all(all_API):
     st.error("Must give API keys")
     st.stop()
 elif all(all_API):
     st.success("API KEYS LOADED SUCCESSFULLY")
-    #===========MODEL=============
+    # ================ MODEL====================
     model = ChatGoogleGenerativeAI(
-       model ='gemini-3.5-flash-lite',
-       google_api_key = GOOGLE_API_KEY
-)
+        model = 'gemini-3.5-flash-lite',
+        google_api_key = GOOGLE_API_KEY
+    )
 else:
-    st.info("PASS ALL API KEYS")
+    st.info("PASS ALL API-KEYS")
+    
 
-#MULTISELECT OPTION
-options =["Delhi","Mumbai","Pune",
-         "Banglore","Gurugram/Gurgaon"]
+# MULTISELECT OPTION
+options = ["Delhi","Mumbai",
+           "Pune","Banglore",
+           "Gurugram/Gurgaon"]
 location = st.sidebar.multiselect("Select Location",
-           options=options)
+                                  options = options)
 
 profile_op = ["Data Analysts","AI Engineer",
               "Gen AI Developer","Full-Stack Dev",
               "Data Scientist"]
-
 profile = st.sidebar.multiselect("Select Job Profile",
-                                  options=profile_op)
-#=======GET USER INFO=======
-st.markdown("""###GET USER INFO""")
-user_info = st.text_area("""Write your Resume Descriprition:""")
-    
+                                  options = profile_op)
 
-# response = model.invoke("Hello Buddy")
+
+# =========GET USER INFO=============
+st.markdown("""### GET USER INFO""")
+user_info = st.text_area("""Write your Resume Description: """)
+
+
+
+
+# response = model.invoke("Hello Buddy!")
 # response.content[-1]['text']
 
+
+# ======================TOOLS===============
 def search_latest_news_jobs(query):
-    """
-    This function helps to fetch latest
-    news or jobs related article using
-    Tavily
-    """
+  """This function helps to fetch latest
+  news or jobs related article using
+  tavily"""
 
-    client = TavilyClient(
-        api_key=TAVILY_API_KEY
-    )
+  client = TavilyClient(
+      api_key = TAVILY_API_KEY)
+  response = client.search(query)
+  return response
 
-    response = client.search(query)
 
-    return response
 
-    agent = create_agent(
+
+# Agent Creation
+agent = create_agent(
     model = model,
     tools = [search_latest_news_jobs])
-#agent
+
+# agent
+
 
 def main_agent(agent, query):
-    """This is main agent, or leader agent
-    orchestrate sub agents"""
+  """This is main agent, or leader agent
+  orchestrate sub agents"""
 
-    # Giving prompt to create detailed prompt
-    # for code generation
-    prompt = """You are AI assistant and
-below given is a prompt, your
-task is to give detailed prompt for
-this.
+  # Giving prompt to create detailed prompt
+  # for code generation
+  prompt = """You are AI assistant and
+  below given is a prompt, your
+  task is to give detailed prompt for
+  this.
+  You are a professional Resume generator
+  where user will give their personal info,
+  you have to create detailed Resume
+  for students or professional one,
+  it must be with dynamic UI and UX and,
+  with advanced CSS Professional Designing
+  Make sure to give output in HTML format only
+  no markdowns allowed
+  """
 
-You are a professional Resume generator
-where user will give their personal info,
-you have to create detailed Resume
-for students or professional one,
-it must be with dynamic UI and UX and,
-with advanced CSS Professional Designing
-Make sure to give output in HTML format only
-no markdowns allowed
-"""
+  response = agent.invoke({'messages':[{'role':'user',
+                                        'content':prompt}]})
+  detailed_prompt = response['messages'][-1].content[-1]['text']
 
-    response = agent.invoke({
-        'messages': [
-            {
-                'role': 'user',
-                'content': prompt
-            }
-        ]
-    })
+  # SAVE PROMPT using File Handling
 
-    detailed_prompt = response["messages"][-1].content[-1]["text"]
+  with open('prompt.txt','w') as f:
+    f.write(detailed_prompt)
 
-    # SAVE PROMPT using File Handling
-    with open("prompt.txt", "w") as f:
-        f.write(detailed_prompt)
+  user_details = f"""Below Given is a user details
+  generate Resume based on that, if not
+  given keep: Default Resume: Python Developer
+  user details: {query}"""
 
-    user_details = f"""Below Given is a user details
-generate Resume based on that, if not
-given keep: Default Resume: Python Developer
-user details: {query}"""
+  final_prompt = prompt + detailed_prompt + user_details
 
-    final_prompt = prompt + detailed_prompt + user_details
+  # CODE GENERATION
+  response = agent.invoke({'messages':[{'role':'user',
+                                        'content':final_prompt}]})
+  code = response['messages'][-1].content[-1]['text']
 
-    # CODE GENERATION
-    response = agent.invoke({
-        'messages': [
-            {
-                'role': 'user',
-                'content': final_prompt
-            }
-        ]
-    })
+  return code
 
-    code = response["messages"][-1].content[-1]["text"]
 
-    return code
+# code = main_agent(agent,"ALAN TURING, GEN AI EXPERT")
+# from IPython import display as DISPLAY
+# DISPLAY.HTML(code)
 
-# code = main_agent(agent, "ALAN TURING, GEN AI EXPERT")
-# from IPython.display import display as DISPLAY,HTML
-# DISPLAY(HTML(code))
 
-def get_jobs(
-        agent,
-        Location="Noida,Delhi",
-        Profile="Data Analysts, AI Engineer"):
 
-    Location = "Noida,Delhi"
-    Profile = "Data Analysts, AI Engineer"
+# Fetch Latest Domain related Jobs using Tavily
 
-    prompt = f"""Based on user given Job profile,
-fetch latest jobs or job apply article
-using Naukri, Linkedin, Indeed, or all popular
-Job apply platforms, Show Results with
-JOB PROFILE NAME, LOCATION, SALARY, COMPANY NAME,
-SHOW jobs only related to given
-{Location} and {Profile}. Output must be in
-Professional HTML Naukri theme cards with Dynamic Design,
-Show atleast Top 10-20 results with direct apply link"""
+def get_jobs(agent,
+             Location,
+             Profile):
+  Location = "Noida,Delhi"
+  Profile = "Data Analysts, AI Engineer"
 
-    response = agent.invoke({
-        'messages': [
-            {
-                'role': 'user',
-                'content': prompt
-            }
-        ]
-    })
+  prompt = f"""Based on user given Job profile,
+  fetch latest jobs or job apply article
+  using Naukri, Linkedin, Indeed, or all popular
+  Job apply platforms, Show Results with
+  JOB PROFILE NAME, LOCATION, SALARY, COMPANY NAME,
+  SHOW jobs only related to given
+  {Location} and {Profile}. Output must be in
+  Professional HTML Naukri theme cards with Dynamic Design,
+  Show atleast Top 10-20 results with direct apply link"""
 
-    code = response['messages'][-1].content[-1]['text']
 
-    return code
+  response = agent.invoke({'messages':[{'role':'user',
+                                          'content':prompt}]})
+  code = response['messages'][-1].content[-1]['text']
+
+  return code
 
 # code = get_jobs(agent)
-# DISPLAY(HTML(code))
+# DISPLAY.HTML(code)
 
 
 if st.button("Generate Resume"):
